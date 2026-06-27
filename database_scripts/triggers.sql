2600331f5604b000b7c1ac1c644d3113c09ea0d9
Ariful Islam <iariful2002@gmail.com>
2026-06-27 23:07:50 +0600

    Implemented Database Triggers

---
+++ database_scripts/triggers.sql
@@ -0,0 +1,107 @@
+-- ============================================
+-- TRIGGERS
+-- ============================================
+
+
+-- TRIGGER 1: DECREASE STOCK ON BORROW
+-- Automatically decrease available stock when a borrow record is inserted.
+
+CREATE OR REPLACE TRIGGER trg_decrease_stock
+AFTER INSERT ON borrow_records
+FOR EACH ROW
+BEGIN
+    UPDATE equipment SET available_quantity = available_quantity - :NEW.quantity
+    WHERE equipment_id = :NEW.equipment_id;
+END;
+/
+
+
+-- TRIGGER 2: INCREASE STOCK ON RETURN
+-- Automatically increase available stock when equipment is returned.
+
+CREATE OR REPLACE TRIGGER trg_increase_stock
+AFTER UPDATE OF borrow_status ON borrow_records
+FOR EACH ROW
+WHEN (NEW.borrow_status = 'RETURNED')
+BEGIN
+    UPDATE equipment SET available_quantity = available_quantity + :NEW.quantity
+    WHERE equipment_id = :NEW.equipment_id;
+END;
+/
+
+
+-- TRIGGER 3: LOG BORROW ACTION
+-- Automatically insert borrow log.
+
+CREATE OR REPLACE TRIGGER trg_log_borrow
+AFTER INSERT ON borrow_records
+FOR EACH ROW
+BEGIN
+    INSERT INTO equipment_logs
+    (
+        log_id,
+        equipment_id,
+        user_id,
+        action_type,
+        quantity,
+        action_date,
+        remarks
+    )
+    VALUES
+    (
+        equipment_logs_seq.NEXTVAL,
+        :NEW.equipment_id,
+        :NEW.user_id,
+        'BORROW',
+        :NEW.quantity,
+        SYSDATE,
+        'Equipment Borrowed'
+    );
+END;
+/
+
+
+-- TRIGGER 4: LOG RETURN ACTION
+-- Automatically insert return log.
+
+CREATE OR REPLACE TRIGGER trg_log_return
+AFTER UPDATE OF borrow_status ON borrow_records
+FOR EACH ROW
+WHEN (NEW.borrow_status = 'RETURNED')
+BEGIN
+    INSERT INTO equipment_logs
+    (
+        log_id,
+        equipment_id,
+        user_id,
+        action_type,
+        quantity,
+        action_date,
+        remarks
+    )
+    VALUES
+    (
+        equipment_logs_seq.NEXTVAL,
+        :NEW.equipment_id,
+        :NEW.user_id,
+        'RETURN',
+        :NEW.quantity,
+        SYSDATE,
+        'Equipment Returned'
+    );
+END;
+/
+
+
+-- TEST 1
+-- Borrow Equipment
+
+INSERT INTO borrow_records VALUES(7010, 5006, 2207045, 'EQ019', 1, SYSDATE, SYSDATE+7, NULL, 'BORROWED');
+
+
+-- TEST 2
+-- Return Equipment
+
+UPDATE borrow_records SET borrow_status='RETURNED', actual_return_date=SYSDATE WHERE borrow_id=7010;
+
+COMMIT;
